cd991d3b99ac6abc7892557a774a90a5af76b32c
Ariane Saulnier <asaulnie@paul-f4Ar4s5.clusters.42paris.fr>
2025-03-07 14:12:30 +0100

    changed text like in subject

--- philo/sources/routine.c
+++ philo/sources/routine.c
@@ -6,36 +6,36 @@
 /*   By: asaulnie <asaulnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 19:57:02 by asaulnie          #+#    #+#             */
-/*   Updated: 2025/03/06 19:59:38 by asaulnie         ###   ########.fr       */
+/*   Updated: 2025/03/07 14:11:54 by asaulnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	think(t_philo *philo)
-{
-	printf("Philosopher %d is thinking...\n", philo->id);
-	usleep(1000);
-}
-
 void	pick_up_forks(t_philo *philo)
 {
-	printf("Philosopher %d is picking up forks...\n", philo->id);
+	printf("%d has taken a fork\n", philo->id);
 }
 
 void	eat(t_philo *philo)
 {
-	printf("Philosopher %d is eating...\n", philo->id);
+	printf("%d is eating\n", philo->id);
 	usleep(1000);
-}
-
-void	put_down_forks(t_philo *philo)
-{
-	printf("Philosopher %d is putting down forks...\n", philo->id);
 }
 
 void	sleep_philosopher(t_philo *philo)
 {
-	printf("Philosopher %d is sleeping...\n", philo->id);
+	printf("%d is sleeping\n", philo->id);
 	usleep(1000);
+}
+
+void	think(t_philo *philo)
+{
+	printf("%d is thinking\n", philo->id);
+	usleep(1000);
+}
+
+void	died(t_philo *philo)
+{
+	printf("%d died\n", philo->id);
 }
